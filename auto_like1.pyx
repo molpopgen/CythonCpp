39ddef884cec0ad1825dcf068e0211a1aac79f1b
Kevin R. Thornton <krthornt@uci.edu>
2016-08-26 14:23:44 -0700

    add the generated cpp code

--- auto_like1.pyx
+++ auto_like1.pyx
@@ -11,6 +11,18 @@
 from libcpp.map cimport map
 from libcpp.vector cimport vector
 
+#The map cannot be passed in as const,
+#otherwise will fail to compile.
+#Looking at the generated .cpp
+#shows that Cython will declare
+#iterator instead of const_iterator
+#if foo is passed in as const:
+#static unsigned int __pyx_f_10auto_like1_iterate_map(std::map<unsigned int,unsigned int>  const &__pyx_v_foo) {
+#  unsigned int __pyx_v_sum1;
+#  std::pair<unsigned int,unsigned int>  __pyx_v_i;
+#  unsigned int __pyx_r;
+#  std::map<unsigned int,unsigned int> ::iterator __pyx_t_1;
+#  std::pair<unsigned int,unsigned int>  __pyx_t_2;
 cdef unsigned iterate_map(map[unsigned,unsigned] & foo) nogil:
 
     cdef unsigned sum1=0
@@ -21,10 +33,23 @@ cdef unsigned iterate_map(map[unsigned,unsigned] & foo) nogil:
 
 #When used with a vector, 
 #and modeling after the iterate_map function,
-#the function must NOT be nogil...:
+#the function must NOT be nogil, else Cython will
+#fail to generate a .cpp file...
 cdef unsigned iterate_vector(vector[unsigned] & foo):
 
     cdef unsigned sum1=0
+    #This code is generating PyObject temporaries,
+    #but the iterate_map function above correctly 
+    #generated the pair as the temporary:
+    #static unsigned int __pyx_f_10auto_like1_iterate_vector(std::vector<unsigned int>  &__pyx_v_foo) {
+    #unsigned int __pyx_v_sum1;
+    #PyObject *__pyx_v_i = NULL;
+    #unsigned int __pyx_r;
+    #__Pyx_RefNannyDeclarations
+    #std::vector<unsigned int> ::iterator __pyx_t_1;
+    #unsigned int __pyx_t_2;
+    #PyObject *__pyx_t_3 = NULL;
+    #PyObject *__pyx_t_4 = NULL;
     for i in foo:
         sum1+=i
 
